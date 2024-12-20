5b7b29bb09398916d001de2ec847b7fcfbe1ace3
Eli Soli <elisoli+git@vivaldi.net>
2024-12-20 13:16:39 -0300

    compile

--- duckduckgo/main.js
+++ duckduckgo/main.js
@@ -1,35 +1,37 @@
-System.register([], function (exports_1, context_1) {
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
     "use strict";
-    var update, log, presence, params, paths, state, details;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [],
-        execute: function () {
-            update = (presence) => chrome.runtime.sendMessage({ target: 'background', type: 'presence', presence });
-            log = (...data) => chrome.runtime.sendMessage({ target: 'background', type: 'log', data });
-            presence = {
-                assets: {
-                    large_image: 'https://duckduckgo.com/assets/logo_header_mobile.alt.v109.svg'
-                },
-                timestamps: { start: Date.now() },
-                buttons: [{ label: 'Search it too', url: window.location.href }]
-            };
-            params = new URLSearchParams(window.location.search);
-            paths = window.location.pathname
-                .replace(/^\/|\/$/, '')
-                .split('/')
-                .filter(e => e);
-            state = () => params.get('q')
-                ? `Searching for ${params.get('q')}`
-                : 'In the home page';
-            details = () => params.get('iaxm') === 'maps' ?
-                'Looking for places in the map'
-                : params.get('ia') && params.get('ia') !== 'web' ?
-                    `Looking for ${params.get('ia')}`
-                    : '';
-            presence.state = state();
-            presence.details = details();
-            update(presence);
-        }
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const update = (presence) => chrome.runtime.sendMessage({ target: 'background', type: 'presence', presence });
+    const log = (...data) => chrome.runtime.sendMessage({ target: 'background', type: 'log', data });
+    const presence = {
+        assets: {
+            large_image: 'https://duckduckgo.com/assets/logo_header_mobile.alt.v109.svg'
+        },
+        timestamps: { start: Date.now() },
+        buttons: [{ label: 'Search it too', url: window.location.href }]
     };
+    const params = new URLSearchParams(window.location.search);
+    const paths = window.location.pathname
+        .replace(/^\/|\/$/, '')
+        .split('/')
+        .filter(e => e);
+    const state = () => params.get('q')
+        ? `Searching for ${params.get('q')}`
+        : 'In the home page';
+    const details = () => params.get('iaxm') === 'maps' ?
+        'Looking for places in the map'
+        : params.get('ia') && params.get('ia') !== 'web' ?
+            `Looking for ${params.get('ia')}`
+            : '';
+    presence.state = state();
+    presence.details = details();
+    update(presence);
 });
